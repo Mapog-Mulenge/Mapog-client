527ac17541ca0eebe473262c07da11af41468a22
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-20 11:01:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
+import Analytics from "./pages/Analytics";
+import Timetable from "./pages/Timetable";
+import Schools from "./pages/Schools";
+import Users from "./pages/Users";
 import Login from "./pages/Login";
-import Dashboard from "./components/Dashboard";
-import TimetablePage from "./pages/TimetablePage";
-import "./styles.css";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/AuthContext";
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/timetable" element={<TimetablePage />} />
-    </Routes>
-  </Router>
-);
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/analytics"
+            element={
+              <ProtectedRoute>
+                <Analytics />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/timetable"
+            element={
+              <ProtectedRoute>
+                <Timetable />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/schools"
+            element={
+              <ProtectedRoute>
+                <Schools />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute>
+                <Users />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
+  );
+}
 
 export default App;
