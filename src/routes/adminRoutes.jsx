0dcc1c0cd127e1c7136fd90a7a630fbd0af69f86
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-20 10:52:45 +0300

    Create adminRoutes.jsx

---
+++ src/routes/adminRoutes.jsx
@@ -0,0 +1,5 @@
+import AnalyticsDashboard from "../pages/Admin/AnalyticsDashboard";
+import PageManagement from "../pages/Admin/PageManagement";
+
+<Route path="/admin/analytics" element={<AnalyticsDashboard />} />
+<Route path="/admin/pages" element={<PageManagement />} />
