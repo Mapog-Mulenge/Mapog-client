ea5a4aead2caba06d204c51d86068f5046916fac
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-20 18:27:14 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['mapog.xyz'],
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://api.mapog.xyz",
   },
 };
 
