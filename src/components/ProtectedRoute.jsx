e0abaf786bed3332edd75de11cd601142032a7cc
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-20 12:01:59 +0300

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
 
-const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" replace />;
+const ProtectedRoute = ({ component: Component }) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? <Component /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
