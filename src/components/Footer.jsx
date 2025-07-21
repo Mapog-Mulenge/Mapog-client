6f336230f0ac0dfdee59ca8e23eb52a1fad4c350
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-21 18:24:59 +0300

    Delete src/components directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4">
-      <p>&copy; {new Date().getFullYear()} Mapog ®. All rights reserved.</p>
-    </footer>
-  );
-}
