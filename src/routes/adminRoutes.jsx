8a1fb60dfc56a09bdb2269a4623a19503e18b20e
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-21 20:00:18 +0300

    Delete src directory

--- src/routes/adminRoutes.jsx
+++
@@ -1,5 +0,0 @@
-import AnalyticsDashboard from "../pages/Admin/AnalyticsDashboard";
-import PageManagement from "../pages/Admin/PageManagement";
-
-<Route path="/admin/analytics" element={<AnalyticsDashboard />} />
-<Route path="/admin/pages" element={<PageManagement />} />
