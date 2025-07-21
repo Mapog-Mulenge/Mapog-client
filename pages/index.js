ff46a1567a464fff323d10a65928851ab5a1e7ca
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-21 20:25:17 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="p-4 text-center">
+        <h1 className="text-4xl font-bold">Welcome to Mapog MSMS</h1>
+        <p className="mt-2 text-gray-500">
+          Streamline your school operations effortlessly.
+        </p>
+      </main>
+      <Footer />
+    </>
+  );
+}
