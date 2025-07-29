f389d6cec21974a522727d70e8ccb9a5a6f25b6a
Mapog-Mulenge <petermulenge@gmail.com>
2025-07-29 21:10:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   distDir: 'dist',
   images: {
     unoptimized: true,
-  }, 
+  },
 };
 
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://api.mapog.xyz",
-      },
+}
 
 module.exports = nextConfig;
